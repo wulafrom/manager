3fd1dfe0a448b51262cb0b20f1ef2548504d26ff
humm <humm@tst.com>
2020-09-23 23:09:49 +0800

    新增部分测试

--- manager-web/src/main/java/com/bjsxt/web/controller/UserController.java
+++ manager-web/src/main/java/com/bjsxt/web/controller/UserController.java
@@ -4,7 +4,9 @@ import com.bjsxt.pojo.Users;
 import com.bjsxt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @version V1.0
@@ -25,6 +27,50 @@ public class UserController {
         //System.out.println(users.getUserage()+" "+users.getUsername()+" "+users.getUserid());
         System.out.println("测试");
         this.userService.addUser(users);
+        return "ok";
+    }
+
+    /**
+     * doGet
+     *
+     * @return
+     */
+    @RequestMapping(value = "/doGet/{userid}",method = RequestMethod.GET)
+    public String doGet(@PathVariable Integer userid){
+       System.out.println("doGet....");
+
+       return "ok";
+    }
+
+    /**
+     *  doPost
+     *
+     * @return
+     */
+    @RequestMapping(value = "/doPost",method = RequestMethod.POST)
+    public String doPost(Users users){
+        System.out.println("doPost....");
+        System.out.println(users.getUserage()+" "+users.getUserid()+" "+users.getUsername());
+        return "ok";
+    }
+
+    /**
+     *  doDelete
+     *
+     * @return
+     */
+    @RequestMapping(value = "/doDelete/{userid}",method = RequestMethod.DELETE)
+    public String doDelete(@PathVariable Integer userid){
+        System.out.println("doDelete....");
+        return "ok";
+    }
+    /**
+     *
+     * @return
+     */
+    @RequestMapping(value = "/doPut",method = RequestMethod.PUT)
+    public String doPut(){
+        System.out.println("doPut....");
         return "ok";
     }
 }
